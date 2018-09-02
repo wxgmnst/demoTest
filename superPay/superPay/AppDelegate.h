551ff9c068a12af480cd03032570909af0917fde
wxgmnst <459599830@qq.com>
2018-09-02 17:47:54 +0800

    初始化项目

---
+++ superPay/superPay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  superPay
+//
+//  Created by wxgmnst on 2017/8/1.
+//  Copyright © 2017年 wxgmnst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ superPay/superPayTests/superPayTests.m
@@ -0,0 +1,39 @@
+//
+//  superPayTests.m
+//  superPayTests
+//
+//  Created by wxgmnst on 2017/8/1.
+//  Copyright © 2017年 wxgmnst. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface superPayTests : XCTestCase
+
+@end
+
+@implementation superPayTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
